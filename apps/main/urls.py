4c151b4476fa75ff28b2c0990a695d4c3737a02f
Your Name <your_email@example.com>
2024-12-13 12:54:50 +0300

    new commit

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact'),
-    path('terms/',views.terms,name='terms'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('terms',views.terms,name='terms'),
 
 ]

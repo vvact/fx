93c7662af4f0c793a1b386e4dc37c57cc66e66f3
Your Name <your_email@example.com>
2024-12-13 12:42:36 +0300

    new commit

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('<slug:slug>', views.blog_detail, name='blog_detail'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]

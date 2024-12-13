5d7ea10588e80d48db3d04cd13b4fbc5721ecc72
Your Name <your_email@example.com>
2024-12-13 12:41:25 +0300

    made changes to the blog section

--- forex/urls.py
+++ forex/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
-    path('blog/', include('apps.blog.urls')),
+    path('blog', include('apps.blog.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
